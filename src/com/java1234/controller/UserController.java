ee58019ee5ed3669ef37491ec2781951a0cf3a84
Mei Zhenhao <2323794414@qq.com>
2018-12-28 14:50:25 +0800

    add all files

---
+++ src/com/java1234/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.java1234.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.java1234.entity.User;
+import com.java1234.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Resource
+	private UserService userService;
+	
+	@RequestMapping("/login")
+	public String login(User user,HttpServletRequest request){
+		User resultUser=userService.login(user);
+		if(resultUser==null){
+			request.setAttribute("user", user);
+			request.setAttribute("errorMsg", "用户名或密码错误！");
+			return "index";
+		}else{
+			HttpSession session=request.getSession();
+			session.setAttribute("currentUser", resultUser);
+			return "success";
+		}
+	}
+	
+	@RequestMapping("/toLogin")
+	public String toLogin(){
+		return "index";
+	}
+}
